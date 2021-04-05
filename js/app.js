c5340e844f6f491eb9b050f679d4f807e7380ee8
henrynoble01 <henrynoble01@gmail.com>
2021-04-05 20:45:49 +0100

    kinda 80% done haha but i just copied the js code

--- js/app.js
+++ js/app.js
@@ -32,5 +32,51 @@ form.addEventListener('submit', e => {
     const email = form['email'].value;
     const password = form['password'].value;
 
-    alert(first + last + email + password);
+    if(firstName === ''){
+        addErrorTo('firstname', 'First Name is required');
+    } else {
+        removeErrorFrom('firstname');
+    }
+
+    if(lastName === ''){
+        addErrorTo('lastname', 'Last Name is required');
+    }else {
+        removeErrorFrom('lastname');
+    }
+
+    if (email === '') {
+        addErrorTo('email', 'Email is required');
+    }else if (!isvalid(email)) {
+         addErrorTo('email', 'Email is not valid');
+    } else {
+        removeErrorFrom('email');
+    }
+
+    // if (isvalid(email)) {
+    //     addErrorTo('email', 'Email is not valid');
+    // }
+
+    if(password === ''){
+        addErrorTo('password', 'password is required');
+    }else {
+        removeErrorFrom('password');
+    }
+
+    // alert(firstName + lastName + email + password);
 });
+
+function addErrorTo(field, message){
+    const small =  form[field].parentNode.querySelector('small');
+    small.innerText = message;
+    small.style.opacity ='1';
+}
+
+function removeErrorFrom(field) {
+    const small =  form[field].parentNode.querySelector('small');
+    small.style.opacity ='0';
+}
+
+function isvalid(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
